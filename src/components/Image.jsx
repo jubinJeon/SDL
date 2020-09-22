0b8a4fcbaba593c92d768c8f93d99fbc2ceec88c
sean <tech@dowidus.com>
2020-09-22 22:58:07 +0900

    삭제

--- src/components/Image.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-
-function Image({src,art,local = false}){
-    return <img src={local ? src : 'http://images.kisvan.co.kr/smartorder/' +  src} art={art}/>
-}
-
-export default Image
--- src/util/PopUpHelper.jsx
+++
@@ -1,6 +0,0 @@
-import React, {useContext} from 'react';
-import {SDLContext} from '../../context/SDLStore'
-
-export  const openAlert  = () => {
-
-}
