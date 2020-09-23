9e313fb4c892ad8244217de32cfc33f1df614f7c
sean <tech@dowidus.com>
2020-09-23 15:25:06 +0900

    특수 문자 치환 함수 수정

--- src/util/Utils.jsx
+++ src/util/Utils.jsx
@@ -409,7 +409,8 @@ export const unescapehtmlcode = (str) => {
 
   return str.replace(/&amp;/gi, "&").replace(/&#35;/gi, "#")
   .replace(/&lt;/gi, "<").replace(/&gt;/gi, ">")
-  .replace(/&quot;/gi, /"/g).replace(/&#39;/gi, '\\')
+  .replace(/&quot;/gi, "\"").replace(/&#34;/gi, "\"")
+  .replace(/&#39;/gi, "'")
   .replace(/&#37;/gi, '%').replace(/&#40;/gi, '(').replace(/&#41;/gi, ')')
   .replace(/&#43;/gi, '+').replace(/&#47;/gi, '/').replace(/&#46;/gi, '.')
   .replace(/&rarr;/gi, '->');
